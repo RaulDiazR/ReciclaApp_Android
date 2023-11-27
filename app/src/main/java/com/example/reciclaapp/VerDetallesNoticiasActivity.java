e1392d3dce79fd6b3ccccfb848251e7e10d2b69f
RaulDiaz <rauldiaz.rom@gmail.com>
2023-11-27 10:33:06 -0600

    Add App icon

--- app/src/main/java/com/example/reciclaapp/VerDetallesNoticiasActivity.java
+++ app/src/main/java/com/example/reciclaapp/VerDetallesNoticiasActivity.java
@@ -1,17 +1,24 @@
 package com.example.reciclaapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
-import android.media.Image;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.style.ImageSpan;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.squareup.picasso.Picasso;
 
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class VerDetallesNoticiasActivity extends AppCompatActivity {
 
@@ -45,6 +52,66 @@ public class VerDetallesNoticiasActivity extends AppCompatActivity {
         titulo.setText(tituloTxt);
         cuerpo.setText(cuerpoTxt);
         autor.setText(autorTxt);
+
+
+        justify(cuerpo);
+    }
+
+    public static void justify(final TextView textView) {
+
+        final AtomicBoolean isJustify = new AtomicBoolean(false);
+
+        final String textString = textView.getText().toString();
+
+        final TextPaint textPaint = textView.getPaint();
+
+        final SpannableStringBuilder builder = new SpannableStringBuilder();
+
+        textView.post(() -> {
+
+            if (!isJustify.get()) {
+
+                final int lineCount = textView.getLineCount();
+                final int textViewWidth = textView.getWidth();
+
+                for (int i = 0; i < lineCount; i++) {
+
+                    int lineStart = textView.getLayout().getLineStart(i);
+                    int lineEnd = textView.getLayout().getLineEnd(i);
+
+                    String lineString = textString.substring(lineStart, lineEnd);
+
+                    if (i == lineCount - 1) {
+                        builder.append(new SpannableString(lineString));
+                        break;
+                    }
+
+                    String trimSpaceText = lineString.trim();
+                    String removeSpaceText = lineString.replaceAll(" ", "");
+
+                    float removeSpaceWidth = textPaint.measureText(removeSpaceText);
+                    float spaceCount = trimSpaceText.length() - removeSpaceText.length();
+
+                    float eachSpaceWidth = (textViewWidth - removeSpaceWidth) / spaceCount;
+
+                    SpannableString spannableString = new SpannableString(lineString);
+                    for (int j = 0; j < trimSpaceText.length(); j++) {
+                        char c = trimSpaceText.charAt(j);
+                        if (c == ' ') {
+                            Drawable drawable = new ColorDrawable(0x00ffffff);
+                            drawable.setBounds(0, 0, (int) eachSpaceWidth, 0);
+                            ImageSpan span = new ImageSpan(drawable);
+                            spannableString.setSpan(span, j, j + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        }
+                    }
+
+                    builder.append(spannableString);
+                }
+
+                textView.setText(builder);
+                isJustify.set(true);
+            }
+        });
     }
 
     // Método llamado cuando se presiona el botón de navegación hacia arriba
