f549730e25112490fc3443e95a87de671e69da20
RaulDiaz <rauldiaz.rom@gmail.com>
2023-11-29 17:26:30 -0600

    Fix Android Manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
